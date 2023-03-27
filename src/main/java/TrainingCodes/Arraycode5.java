b624552f94771eb623c41504ba2131823b7ce177
Sachin276 <sachinbldeacet@gmail.com>
2023-03-27 13:01:23 +0530

    linear search in array

---
+++ src/main/java/TrainingCodes/Arraycode5.java
@@ -0,0 +1,35 @@
+package TrainingCodes;
+//linear search
+
+import java.util.Scanner; 
+public class Arraycode5 
+{
+	public static int linearsearch(int arr[],int key)
+	{
+		for(int i=0;i<arr.length;i++) 
+		{
+			if(arr[i]==key) 
+			{
+				return (i+1);
+			}
+		}
+		return 0;
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the array element to be searched:");
+		int key=sc.nextInt();
+		int arr[]= {10,30,25,68,40,88,99,45,48,96,4,8};
+		int res=linearsearch(arr,key);
+		if(res>0) 
+		{
+		System.out.println("elemeny found in position:"+(res));
+		}
+		else 
+		{
+			System.out.println("element not found:");
+		}
+	
+	}
+}
